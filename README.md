9ed2da0a0f13d2e2067f254fc722fc0f4fac46f1
darika <darikapiterski@gmail.com>
2024-07-23 20:48:02 +0300

    bugfix: БД изменена на SQLite. Внесены правки в шаблоны. Исправлена инструкция в README

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-Разворачивание проекта стандартное:
-`php artisan migrate --seed
+Установлен SqLite. Перед миграциями войти в терминале в корень проекта и запустить
+
+`touch database/user-service.sqlite`
+
+Потом
+
+`php artisan migrate
 `
+
+После стандартное разворчивание
+
 `php artisan serve
 `
+
 `npm run dev `
 
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,64 +1,66 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Добавление пользователя</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('users.index') }}"> Назад</a>
-            </div>
-        </div>
-    </div>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form action="{{ route('users.store') }}" method="POST">
-        @csrf
-
+    <div class="col-md-12">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Имя:</strong>
-                    <input type="text" name="name" class="form-control" placeholder="Введите имя">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Добавление пользователя</h2>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>EMail:</strong>
-                    <input type="email" name="email" class="form-control" placeholder="Введите email">
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('users.index') }}"> Назад</a>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Пароль:</strong>
-                    <input type="password" name="password" class="form-control" placeholder="Введите пароль">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Повторите пароль:</strong>
-                    <input type="password" name="password_confirmation" class="form-control" placeholder="Введите пароль еще раз">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Заблокировать:</strong>
-                    <input type="checkbox" name="isBlocked">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Отправить</button>
             </div>
         </div>
-    </form>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('users.store') }}" method="POST">
+            @csrf
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Имя:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Введите имя">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>EMail:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="Введите email">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Пароль:</strong>
+                        <input type="password" name="password" class="form-control" placeholder="Введите пароль">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Повторите пароль:</strong>
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Введите пароль еще раз">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Заблокировать:</strong>
+                        <input type="checkbox" name="isBlocked">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Отправить</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection
